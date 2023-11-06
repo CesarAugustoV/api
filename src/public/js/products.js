d85abb0e7834c064eb9305bae1eb4a3b7829b2bc
cesaraugustovicci@icloud.com <cesaraugustovicci@icloud.com>
2023-11-06 11:08:56 -0300

    id added

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -4,12 +4,18 @@
     const actualPage = document.querySelector('.current-page');
 
     function createProductCard(product) {
+
+        const productIdInput = document.createElement('input');
+        productIdInput.type = 'hidden';
+        productIdInput.name = 'product-id'; // Puedes establecer un nombre
+        productIdInput.value = product._id;
+
         const card = document.createElement('div');
         card.className = 'product-card';
 
         const image = document.createElement('div');
         image.className = 'product-image';
-        // Agregar una imagen del producto (puedes personalizar esto)
+        // Agregar una imagen del producto
 
         const title = document.createElement('div');
         title.className = 'product-title';
@@ -35,6 +41,7 @@
         card.appendChild(description);
         card.appendChild(price);
         card.appendChild(addToCart);
+        card.appendChild(productIdInput);
 
         return card;
     }
@@ -54,7 +61,7 @@
             return response.json(); // Convierte la respuesta a formato JSON
         })
         .then(data => {
-            // Haz algo con los datos recibidos, por ejemplo, mostrarlos en la consola
+
             data.products.results.forEach(product => {
                 const productCard = createProductCard(product);
                 productContainer.appendChild(productCard);
@@ -63,71 +70,18 @@
             //paginacion
             const info = data.products.info;
 
-            if(!info.prevPage){
-                prevPage.disabled= true;
+            if (!info.prevPage) {
+                prevPage.disabled = true;
             }
 
-            if(!info.nextPage){
-                nextPage.disabled= true;
+            if (!info.nextPage) {
+                nextPage.disabled = true;
             }
-            
-            actualPage.innerHTML=info.actualPage;
-            totalPages.innerHTML=info.totalPages;
+
+            actualPage.innerHTML = info.actualPage;
+            totalPages.innerHTML = info.totalPages;
 
         })
         .catch(error => {
             console.error('Ocurrió un error: ' + error);
         });
-
-
-    // Función para crear un carrito
-    function createCart() {
-        fetch('http://localhost:8080/api/carts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({}) // Puedes enviar información adicional si es necesario
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('No se pudo crear el carrito');
-                }
-                return response.json();
-            })
-            .then(cart => {
-                // Una vez que se crea el carrito, agrega el producto al carrito
-                addProductToCart(cart._id);
-            })
-            .catch(error => {
-                console.error('Ocurrió un error al crear el carrito: ' + error);
-            });
-    }
-
-    // Función para agregar un producto al carrito
-    function addProductToCart(idCart) {
-        fetch(`http://localhost:8080/api/carts/${idCart}/products/${idProducto}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    quantity: 1
-                }) // Puedes ajustar la cantidad según tus necesidades
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('No se pudo agregar el producto al carrito');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Producto agregado exitosamente al carrito:', data);
-            })
-            .catch(error => {
-                console.error('Ocurrió un error al agregar el producto al carrito: ' + error);
-            });
-    }
-
-    // Llama a la función para crear el carrito
-    createCart();
